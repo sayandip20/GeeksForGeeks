dfc70470ac1c4dd5d1cd4688dc82d9295233fe61
Sayandip Adhikari <88745726+sayandip20@users.noreply.github.com>
2023-08-17 13:42:53 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Next Smallest Palindrome/next-smallest-palindrome.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+class Solution:
+
+	def generateNextPalindrome(self,nums, n ) :
+	    # code here
+        s=(n-1)//2
+        f=1 
+        for i in range(s,-1,-1):
+            l=nums[i]-nums[n-i-1]
+            if l:
+                f=1 if l<0 else 0
+                break;
+        if f:                   
+            for i in range(s,-1,-1):
+                if f==0:  break
+                f,nums[i]=divmod(nums[i]+1,10)
+            if f:
+                nums=[1]+nums
+                n=n+1
+        for i in range(s+1):
+                nums[n-i-1]=nums[i]
+           
+        return nums
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+# Driver code 
+if __name__ == "__main__": 		
+    tc=int(input())
+    while tc > 0:
+        n=int(input())
+        num=list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.generateNextPalindrome(num, n)
+        for x in ans:
+            print(x, end=" ")
+        print()
+        tc=tc-1
+# } Driver Code Ends
