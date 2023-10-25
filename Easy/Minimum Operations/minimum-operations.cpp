ea94551303ff7e4d61f912526c39a6714e1b9870
Sayandip Adhikari <88745726+sayandip20@users.noreply.github.com>
2023-10-26 00:10:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Operations/minimum-operations.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        //code here.
+        int operations = 0;
+        while(n) {
+            if(n%2 == 0)
+                n /= 2;
+            else
+                n -= 1;
+            operations++;
+        }
+        return operations;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
