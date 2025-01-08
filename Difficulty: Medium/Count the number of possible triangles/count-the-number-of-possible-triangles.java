ccfa35c012c7138799dfe25f0006c4592c29f94f
Sayandip Adhikari <88745726+sayandip20@users.noreply.github.com>
2025-01-08 23:52:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            System.out.println(new Solution().countTriangles(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to count the number of possible triangles.
+    static int countTriangles(int arr[]) {
+        // code here
+        int n = arr.length;
+        if (n < 3) return 0;
+        Arrays.sort(arr);
+        int count = 0;
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+
+            while (i < j) {
+                if (arr[i] + arr[j] > arr[k]) {
+                    count += (j - i);
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
